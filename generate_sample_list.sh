e4475f07965bafe92b6c074530dafd215cfb3369
Cameron Jeffries <cameron.jeffries@terazo.com>
2022-05-25 21:09:50 -0500

    adding batch method

--- generate_sample_list.sh
+++ generate_sample_list.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 dir="./split_fastq"
-
 samples=$(ls $dir/ | cut -d'.' -f 1 | uniq | sort)
 
 for sample in $samples; do
     echo "Running Task: $sample"
+    ## Run one at a time
     ./run_sample.sh $sample
+    ## Run all at once
+    sbatch ./run_sample.sh $sample &
     echo "***********"
 done
